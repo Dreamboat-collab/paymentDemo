8dec2303d705293b120960ca724b387d6b62c7b5
jeddy <1556782359@qq.com>
2026-01-06 14:32:58 +0800

    前端以及支付参数

--- src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
+++ src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
@@ -7,12 +7,14 @@ import com.atguigu.paymentdemo.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/product")
 @Api(tags = "商品管理")
